62da08aebe827f3ac276d12d95b04e1be46bc20b
akim305 <akim305@gatech.edu>
2025-01-24 00:21:11 -0500

    default tensor type

--- src/crosscoders/__init__.py
+++ src/crosscoders/__init__.py
@@ -10,3 +10,6 @@ from crosscoders import abc
 from crosscoders import autoencoders
 # from crosscoders import constants
 from crosscoders import configs
+
+import torch
+torch.set_default_dtype(torch.float32)
