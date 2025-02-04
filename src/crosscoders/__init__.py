8004aa6d4fc44189934c6d3ea5a01e6f1007318a
xpkcs <ktxandy@proton.me>
2025-02-04 20:17:16 +0000

    updated init order

--- src/crosscoders/__init__.py
+++ src/crosscoders/__init__.py
@@ -2,14 +2,12 @@
 
 
 
-# import first
-from crosscoders.constants import CONSTANTS
-
-
+# import order matters
 from crosscoders import abc
-from crosscoders import autoencoders
-# from crosscoders import constants
 from crosscoders import configs
+from crosscoders.constants import CONSTANTS
+from crosscoders import autoencoders
+
 
 import torch
 torch.set_default_dtype(torch.float32)
