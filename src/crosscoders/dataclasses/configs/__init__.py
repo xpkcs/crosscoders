d0145ba7f8d74808fa9dd6e0fd33a3e2976a713c
xpkcs <ktxandy@proton.me>
2025-02-04 20:17:54 -0500

    merged runner relevant configs into file

--- src/crosscoders/dataclasses/configs/__init__.py
+++ src/crosscoders/dataclasses/configs/__init__.py
@@ -2,12 +2,16 @@
 
 
 
-# from crosscoders.configs.hardware import HardwareConfig
-from crosscoders.dataclasses.configs.globals import GlobalsConfig, HardwareConfig, ExperimentConfig
+from crosscoders.dataclasses.configs.globals import (
+    HardwareConfig,
+    ExperimentConfig,
+    GlobalsConfig
+)
 
-from crosscoders.dataclasses.configs.optimizer import OptimizerConfig
-from crosscoders.dataclasses.configs.model import ModelConfig
-from crosscoders.dataclasses.configs.runner import AutoencoderLightningModuleConfig
-# from crosscoders.configs.experiment import ExperimentConfig
+from crosscoders.dataclasses.configs.runner import (
+    ModelConfig,
+    OptimizerConfig,
+    AutoencoderLightningModuleConfig
+)
 
 from crosscoders.dataclasses.metrics.loss import LossMetrics
