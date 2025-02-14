ccc21c2a65dc2f25c62ca896e2de2394ec8a8d67
xpkcs <ktxandy@proton.me>
2025-02-14 21:52:28 +0000

    enhanced runner

--- src/crosscoders/autoencoders/acausal/runner.py
+++ src/crosscoders/autoencoders/acausal/runner.py
@@ -4,6 +4,8 @@
 
 import datetime
 from functools import cached_property
+import os
+import tempfile
 from typing import Dict, Optional
 import einops
 import ray, ray.train
@@ -20,13 +22,15 @@ from torch.utils.tensorboard import SummaryWriter
 
 
 
-class AcausalAutoencoderRunner(AutoencoderRunnerABC, AcausalLoss):
+class AcausalAutoencoderRunner(AutoencoderRunnerABC):
 
     def __init__(self, cfg: RunnerConfig) -> None:
 
         super().__init__(cfg)
 
         self.model: AcausalAutoencoder = AcausalAutoencoder(self.cfg.MODEL)
+
+        self.loss: AcausalLoss = AcausalLoss(self.cfg.LOSS)
 
         self.optimizer = self.configure_optimizers()
 
@@ -39,40 +43,60 @@ class AcausalAutoencoderRunner(AutoencoderRunnerABC, AcausalLoss):
         x = batch['resid_post']
         self.num_tokens_processed += x.shape[0]
         
-        outputs = self.model(x)
-        loss = self.loss(outputs, x, W_dec=self.model.W_dec, x_enc=self.model.x_enc, L1_COEFFICIENT=self.cfg.LOSS.L1_COEFFICIENT)
-        loss.loss.backward()
+        x_hat = self.model(x)
+        loss, metrics = self.loss(x, x_hat, self.model.x_enc, self.model.W_dec)
+        loss.backward()
         
         total_grad_norm = torch.nn.utils.clip_grad_norm_(self.model.parameters(), 1)
-        self.writer.add_scalar(f'total_grad_norm', total_grad_norm, self.num_tokens_processed)
+        self.writer.add_scalar(f'eval/total_grad_norm', total_grad_norm, self.num_tokens_processed)
         
         self.optimizer.step()
         self.optimizer.zero_grad()
 
-        self.writer.add_scalar(f'loss/loss', loss.loss.item(), self.num_tokens_processed)
-        self.writer.add_scalar(f'loss/error', loss.error.item(), self.num_tokens_processed)
-        self.writer.add_scalar(f'loss/l1', loss.l1.item(), self.num_tokens_processed)
-        self.writer.add_scalar(f'loss/l0', loss.l0.item(), self.num_tokens_processed)
+        self.writer.add_scalar(f'loss/loss', metrics.loss, self.num_tokens_processed)
+        self.writer.add_scalar(f'loss/error', metrics.error, self.num_tokens_processed)
+        self.writer.add_scalar(f'loss/l1', metrics.l1, self.num_tokens_processed)
+        self.writer.add_scalar(f'loss/l0', metrics.l0, self.num_tokens_processed)
+        self.writer.add_scalar(f'eval/explained_variance', metrics.explained_variance, self.num_tokens_processed)
+        self.writer.add_scalar(f'eval/dead_neurons/all_tokens', metrics.dead_neurons.all_tokens, self.num_tokens_processed)
+        self.writer.add_scalar(f'eval/dead_neurons/one_token', metrics.dead_neurons.one_token, self.num_tokens_processed)
+        self.writer.add_scalar(f'eval/dead_neurons/no_token', metrics.dead_neurons.no_token, self.num_tokens_processed)
 
-        return loss
+        return metrics
 
 
     def fit(self, dl):
         
         for batch_idx, batch in enumerate(dl):
 
-            loss = self.training_step(batch)
+            metrics = self.training_step(batch)
+            metrics_dict = {
+                'loss': metrics.loss,
+                'error': metrics.error,
+                'l1': metrics.l1,
+                'l0': metrics.l0,
+                'explained_variance': metrics.explained_variance,
+                'dead_neurons/all_tokens': metrics.dead_neurons.all_tokens,
+                'dead_neurons/one_token': metrics.dead_neurons.one_token,
+                'dead_neurons/no_token': metrics.dead_neurons.no_token,
+                'n_tokens': self.num_tokens_processed,
+            }
 
             # if batch_idx % 10 == 0:
             ray.train.report(
-                {
-                    'loss': loss.loss.item(),
-                    'error': loss.error.item(),
-                    'l1': loss.l1.item(),
-                    'l0': loss.l0.item(),
-                    'n_tokens': self.num_tokens_processed,
-                },
+                metrics_dict,
             )
 
 
-        return loss
+        with tempfile.TemporaryDirectory() as temp_checkpoint_dir:
+            torch.save(
+                self.model.state_dict(),
+                os.path.join(temp_checkpoint_dir, "model.pt")
+            )
+            ray.train.report(
+                metrics_dict,
+                checkpoint=ray.train.Checkpoint.from_directory(temp_checkpoint_dir),
+            )
+
+
+        return metrics
