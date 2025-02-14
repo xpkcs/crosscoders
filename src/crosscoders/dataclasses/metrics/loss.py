6f1448314ff5ee66510a35ec5c1e6a5718243909
xpkcs <ktxandy@proton.me>
2025-02-14 21:49:58 +0000

    added metrics to config

--- src/crosscoders/dataclasses/metrics/loss.py
+++ src/crosscoders/dataclasses/metrics/loss.py
@@ -13,6 +13,15 @@ from crosscoders.abc.dataclass import DataclassABC
 
 
 @dataclass(repr=False)
+class DeadNeuronMetrics(DataclassABC):
+    
+    all_tokens: float
+    one_token : float
+    no_token  : float
+
+
+
+@dataclass(repr=False)
 class LossMetrics(DataclassABC):
 
     loss: torch.Tensor       # only required output to run backward()
@@ -20,6 +29,9 @@ class LossMetrics(DataclassABC):
     error: torch.Tensor
     l1: torch.Tensor
     l0: torch.Tensor
+
+    explained_variance: float
+    dead_neurons: DeadNeuronMetrics
 
 
     # def asdict(self):
