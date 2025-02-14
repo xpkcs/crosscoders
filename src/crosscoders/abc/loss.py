77a2327c1a53ff26d48b3382a16d45fb2db34a8c
xpkcs <ktxandy@proton.me>
2025-02-14 21:47:35 +0000

    added additional metrics

--- src/crosscoders/abc/loss.py
+++ src/crosscoders/abc/loss.py
@@ -22,3 +22,24 @@ class LossABC(BaseABC):
     def __call__(self, x: torch.Tensor, x_hat: torch.Tensor, **kwargs: dict) -> LossMetrics:
         ...
 
+
+    @staticmethod
+    def explained_variance(x: torch.Tensor, x_hat: torch.Tensor) -> float:
+
+        # (bs, nl, dm) -> (nl, dm) -> 1
+        # can we assume independence to sum vars?
+        variance = x.var(dim=0).sum()
+        residual_variance = (x - x_hat).var(dim=0).sum()
+
+        return (1 - (residual_variance / variance)).item()
+    
+
+    @staticmethod
+    def dead_neurons(x_enc: torch.Tensor):
+
+        # why use any? why not count how many tokens each neuron is dead for?
+        all_tokens = x_enc.all(dim=0).sum()             # fires on all tokens
+        one_token  = x_enc.any(dim=0).sum()             # fires on at least one token
+        no_token   = (x_enc == 0).all(dim=0).sum()      # fires on no tokens
+
+        return DeadNeuronMetrics(*map(lambda _: (_ / x_enc.shape[-1]).item(), (all_tokens, one_token, no_token)))
