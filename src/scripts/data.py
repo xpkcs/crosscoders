bd6722cdaf0bdf03add09dc79dd300003af5c9aa
xpkcs <ktxandy@proton.me>
2025-02-23 00:43:43 +0000

    idk stuff

--- src/scripts/data.py
+++ src/scripts/data.py
@@ -6,7 +6,9 @@ from crosscoders.data.dataset import TinyStoriesRayDataset
 
 def main():
 
-    train_ds = TinyStoriesRayDataset(s3_prefix='tiny-stories-33M/mlp_out/')
+    # file_slice = (0, 100)
+    # train_ds = TinyStoriesRayDataset(s3_prefix=f'tiny-stories-33M/chunks/{file_slice[0]}-{file_slice[1]}/')
+    train_ds = TinyStoriesRayDataset(s3_prefix='tiny-stories-33M/all/')
     train_ds.save(train_ds.load())
 
 
