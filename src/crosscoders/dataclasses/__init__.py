1c200c54b87303ec14605c2d629074d0b453640f
xpkcs <ktxandy@proton.me>
2025-04-03 16:10:27 -0400

    commenting out convoluted stuff

--- src/crosscoders/dataclasses/__init__.py
+++ src/crosscoders/dataclasses/__init__.py
@@ -4,14 +4,14 @@ from crosscoders.dataclasses.config import *
 from crosscoders.dataclasses.runner import *
 from crosscoders.dataclasses.autoencoders import *
 
-from crosscoders.dataclasses.config import __all__ as __config_all__
-from crosscoders.dataclasses.runner import __all__ as __runner_all__
-from crosscoders.dataclasses.autoencoders import __all__ as __autoencoders_all__
+# from crosscoders.dataclasses.config import __all__ as __config_all__
+# from crosscoders.dataclasses.runner import __all__ as __runner_all__
+# from crosscoders.dataclasses.autoencoders import __all__ as __autoencoders_all__
 
 
-__all__ = []
-__all__ += [
-    object
-    for _ in (__config_all__, __runner_all__, __autoencoders_all__)
-    for object in _
-]
+# __all__ = []
+# __all__ += [
+#     object
+#     for _ in (__config_all__, __runner_all__, __autoencoders_all__)
+#     for object in _
+# ]
